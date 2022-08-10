6f0a667a850425684ca7b9705c7b9d3ffa906d08
Sandycheeks0608 <110962269+Sandycheeks0608@users.noreply.github.com>
2022-08-10 15:13:13 +0900

    main.blocks, main.ts 업데이트하기

--- main.ts
+++ main.ts
@@ -1,14 +1,16 @@
-let 나이 = 0
 input.onButtonPressed(Button.A, function () {
     나이 += 1
 })
 input.onButtonPressed(Button.AB, function () {
-    let 생년 = 0
     나이 = 2022 - 생년
 })
 input.onButtonPressed(Button.B, function () {
     나이 += -1
 })
+let 생년 = 0
+let 나이 = 0
+나이 = 0
+생년 = 2007
 basic.forever(function () {
     basic.showString("Yang")
     basic.pause(1000)
